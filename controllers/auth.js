6396a7c13197209a7153a71d42972ef2b4aea6d5
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-04-23 17:29:29 +0200

    add send emails function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,194 +1,187 @@
-const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middleware/async');
-const User = require('../models/User');
-const crypto = require('crypto')
-const Email = require('./../utils/sendEmail');
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
+const crypto = require("crypto");
+const sendEmail = require("./../utils/sendEmail");
+
+
 
 //@desc       Register User
 //@route      GET/api/v1/auth/register
 //@access     public
 exports.register = asyncHandler(async (req, res, next) => {
-    const { password, confirmPassword, role } = req.body;
+  const { password, confirmPassword, role } = req.body;
 
-    if (role === 'admin')
-        return next(new ErrorResponse('Register as admin is not allowed'), 400)
+  if (role === "admin")
+    return next(new ErrorResponse("Register as admin is not allowed"), 400);
 
-    if (password !== confirmPassword)
-        return next(
-            new ErrorResponse(`password and confirm password don't match`, 400)
-        );
-    //create user
-    const user = await User.create(req.body);
+  if (password !== confirmPassword)
+    return next(
+      new ErrorResponse(`password and confirm password don't match`, 400)
+    );
+  //create user
+  const user = await User.create(req.body);
 
-    //const url = `${req.protocol}://${req.get('host')}/profile`;
+  //const url = `${req.protocol}://${req.get('host')}/profile`;
 
-    //await new Email(user, url).sendWelcome();
+  //await new Email(user, url).sendWelcome();
 
-    sendTokenResponse(user, 200, req, res);
-
-})
+  sendTokenResponse(user, 200, req, res);
+});
 
 // @desc      Login user
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    // Validate emil & password
-    if (!email || !password) {
-        return next(new ErrorResponse('Please provide an email and password', 400));
-    }
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
 
-    // Check for user
-    const user = await User.findOne({ email }).select('+password');
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
 
-    if (!user) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-    // Check if password matches
-    const isMatch = await user.matchPassword(password);
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
 
-    if (!isMatch) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
-    sendTokenResponse(user, 200, req, res);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  sendTokenResponse(user, 200, req, res);
 });
-
 
 // @desc      Forgot password
 // @route     POST /api/v1/auth/forgotPassword
 // @access    Public
 exports.forgetPassword = asyncHandler(async (req, res, next) => {
-    const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
 
-    if (!user) {
-        return next(new ErrorResponse('There is no user with that email', 404));
-    }
+  if (!user) {
+    return next(new ErrorResponse("There is no user with that email", 404));
+  }
 
-    // Get reset token
-    const resetToken = user.getResetPasswordToken();
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
 
-    // save hashed token and expire date
-    await user.save({ validateBeforeSave: false }); //to stop run validation which will ask to enter required fields
+  // save hashed token and expire date
+  await user.save({ validateBeforeSave: false });
+  //to stop run validation which will ask to enter required fields
 
+  try {
+    // Create reset url
+    const message = `${req.protocol}://${req.get(
+      "host"
+    )}/api/v1/auth/resetPassword/${resetToken}`;
 
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    });
 
+    res.status(200).json({ success: true, msg: "Email sent" });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
-    try {
-        // Create reset url
-        const resetURL = `${req.protocol}://${req.get(
-            'host'
-        )}/api/v1/auth/resetPassword/${resetToken}`;
+    await user.save({ validateBeforeSave: false });
 
-        await new Email(user, resetURL).sendPasswordReset();
-
-        res.status(200).json({ success: true, msg: 'Email sent' });
-    } catch (err) {
-        console.log(err);
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpire = undefined;
-
-        await user.save({ validateBeforeSave: false });
-
-        return next(new ErrorResponse('Email could not be sent', 500));
-    }
-
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
 });
 
 // @desc      Reset password
 // @route     PUT /api/v1/auth/resetpassword/:resettoken
 // @access    Public
 exports.resetPassword = asyncHandler(async (req, res, next) => {
-    // Get hashed token
-    const resetPasswordToken = crypto
-        .createHash('sha256')
-        .update(req.params.resettoken)
-        .digest('hex');
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resettoken)
+    .digest("hex");
 
-    const user = await User.findOne({
-        resetPasswordToken,
-        resetPasswordExpire: { $gt: Date.now() }
-    });
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
 
-    if (!user) {
-        return next(new ErrorResponse('Invalid token', 400));
-    }
+  if (!user) {
+    return next(new ErrorResponse("Invalid token", 400));
+  }
 
-    // Set new password
-    user.password = req.body.password;
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-    await user.save();
-    sendTokenResponse(user, 200, req, res);
+  // Set new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+  sendTokenResponse(user, 200, req, res);
 });
-
 
 // @desc      Update password
 // @route     PUT /api/v1/auth/updatePassword
 // @access    Private
 exports.updatePassword = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id).select('+password');
+  const user = await User.findById(req.user.id).select("+password");
 
-    // Check current password
-    if (!(await user.matchPassword(req.body.currentPassword))) {
-        return next(new ErrorResponse('Password is incorrect', 401));
-    }
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
 
-    user.password = req.body.newPassword;
-    await user.save();
+  user.password = req.body.newPassword;
+  await user.save();
 
-    sendTokenResponse(user, 200, req, res);
+  sendTokenResponse(user, 200, req, res);
 });
 
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
 // @access    Private
 exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json({
-        success: true,
-        data: user
-    });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
-
 
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, req, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
+  // Create token
+  const token = user.getSignedJwtToken();
 
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRE
-        ),
-        httpOnly: true,
-        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-    };
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  };
 
-    res
-        .status(statusCode)
-        .cookie('token', token, options)
-        .json({
-            success: true,
-            token
-        });
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
 };
-
 
 // @desc      Log user out / clear cookie
 // @route     GET /api/v1/auth/logout
 // @access    Public
 exports.logout = asyncHandler(async (req, res, next) => {
-    res.cookie('token', 'none', {
-        expires: new Date(Date.now() + 10 * 1000), //10 sec
-        httpOnly: true
-    });
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000), //10 sec
+    httpOnly: true,
+  });
 
-    res.status(200).json({
-        success: true,
-        data: {}
-    });
-    console.log(res.cookies)
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+  console.log(res.cookies);
 });
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,71 +1,35 @@
-const nodemailer = require('nodemailer');
-const pug = require('pug');
-const htmlToText = require('html-to-text');
-const dotenv = require('dotenv');
+const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
+const dotenv = require("dotenv");
 
 // Load env vars
-dotenv.config({ path: '../config/.env' });
+dotenv.config({ path: "../config/.env" });
 
-module.exports = class Email {
-  constructor(user, url) {
-    this.to = user.email;
-    this.firstName = user.name.split(' ')[0];
-    this.url = url;
-    this.from = `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`;
-  }
 
-  newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      return nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SEND_GRID_USERNAME,
-          pass: process.env.SEND_GRID_PASSWORD
-        }
-      });
-    }
-    return nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_PASSWORD
-      }
-    });
-  }
 
-  // Send the actual email
-  async send(template, subject) {
-    // 1) Render HTML based on a pug template
-    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
-      firstName: this.firstName,
-      url: this.url,
-      subject
-    });
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-    // 2) Define email options
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html,
-      text: htmlToText.fromString(html)
-    };
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
 
-    // 3) Create a transport and send email
-    await this.newTransport().sendMail(mailOptions);
-  }
+  const info = await transporter.sendMail(message);
 
-  async sendWelcome() {
-    await this.send('welcome', 'Welcome to the YallaTourism Family!');
-  }
-
-  async sendPasswordReset() {
-    await this.send(
-      'passwordReset',
-      'Your password reset token (valid for only 10 minutes)'
-    );
-  }
-
+  console.log("Message sent: %s", info.messageId);
 };
+
+module.exports = sendEmail;
+
+
