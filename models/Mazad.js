bbdbd4e11b4846a15b30ba7aac7be6e4d6cfd584
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-05-25 18:54:29 +0200

    add Mazad Model

---
+++ models/Mazad.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const MazadSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: [30, "Please add a name for Mazad"],
+    },
+    describtion: {
+      type: String,
+      required: true,
+      maxlength: [50, "Please add describtion for Mazad"],
+    },
+    start_price: {
+      type: Number,
+      required: true,
+    },
+    market_price: {
+      type: Number,
+      required: true,
+    },
+    expected_peice: {
+      type: Number,
+      required: true,
+    },
+    current_price: {
+      type: Number,
+    },
+    start_time: {
+      type: Date,
+      required: true,
+    },
+    end_time: {
+      type: Date,
+      required: true,
+    },
+    interested_subscribers: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
+    subscribers: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
+    merchant: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Mazad must belong to Merchant"],
+    },
+    winner: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+module.exports = mongoose.model("Mazad", MazadSchema);
