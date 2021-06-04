52bddf8d753a3fd2b2b5fe9c469dbfdb92219225
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-06-04 03:33:27 +0200

    add Merchant stats endpoint

--- controllers/statistics.js
+++ controllers/statistics.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 const Request = require("../models/Request");
 const Mazad = require("../models/Mazad");
-const { request } = require("express");
+const TimeNow = require("../utils/GetCurrentTime");
 
 // @desc      Get Statistics
 // @route     GET /api/v1/statist
@@ -33,5 +33,34 @@ exports.getStatist = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: statis,
+  });
+});
+
+// @desc      Get Statistics
+// @route     GET /api/v1/statist/merchant_statist
+// @access    Private/Merchant
+exports.merchantStatis = asyncHandler(async (req, res, next) => {
+  const Mazads = await Mazad.find({ merchant: req.user.id });
+
+  let currentMazads = 0;
+  let finishedMazads = 0;
+  let upcomingMazads = 0;
+  let currentTime = TimeNow();
+
+  Mazads.forEach((el) => {
+    if (el.start_time > currentTime) upcomingMazads = upcomingMazads + 1;
+    else if (el.start_time <= currentTime && currentTime < el.end_time)
+      currentMazads = currentMazads + 1;
+    else if (el.end_time <= currentTime) finishedMazads = finishedMazads + 1;
+  });
+  var obj = {};
+  obj.currentMazads = currentMazads;
+  obj.finishedMazads = finishedMazads;
+  obj.upcomingMazads = upcomingMazads;
+  obj.total = currentMazads + finishedMazads + upcomingMazads;
+
+  res.status(200).json({
+    success: true,
+    data: obj,
   });
 });
--- routes/statist.js
+++ routes/statist.js
@@ -1,16 +1,13 @@
 const express = require("express");
-const {
-    getStatist
-} = require("../controllers/statistics");
-
-
+const { getStatist, merchantStatis } = require("../controllers/statistics");
 
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 
-router.use(protect, authorize("admin"));
+router.use(protect, authorize("admin", "merchant"));
 
 router.route("/").get(getStatist);
+router.route("/merchant_statist").get(merchantStatis);
 
 module.exports = router;
